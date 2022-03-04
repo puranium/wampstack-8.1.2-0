435bde57bce1438b9282efa9b30a47e2358344cf
Puranium Lee <puranium235@gmail.com>
2022-03-05 02:39:29 +0900

    Mat1 이름 수정

--- apache2/htdocs/index.py
+++ apache2/htdocs/index.py
@@ -1,32 +1,45 @@
 #!D:\anaconda3\python.exe
-print("Content-Type: text/html")    # HTML is following
+import cgi
+import os
+
+print("Content-Type: text/html")  # HTML is following
 print()
+
+files = os.listdir('data')
+CharList = ''
+for item in files:
+    CharList = CharList + '<a href="index.py?id={char_name}" title="{char_name}"><li id="char">{char_name}</li></a>'.format(
+        char_name=item)
+
+form = cgi.FieldStorage()
+if 'id' in form:
+    page_id = form["id"].value
+    description = open('data/' + page_id, 'r', encoding='utf-8').read()
+else:
+    page_id = 'Welcome'
+    description = ' <- 클릭해주세요'
 
 print('''<!DOCTYPE html>
 <html lang="ko">
 <head>
-  <meta charset="UTF-8">
+  <meta charset="utf-8-sig">
   <title>원신 돌파재료 정보</title>
-  <link rel="stylesheet" href="css/table-style.css">
+  <link rel="stylesheet" href="table-style.css">
+
 </head>
 <body>
 <div id="wrapper">
   <div id="header">
-    <h1><a href="home.html" title="캐릭별 돌파/특성 재료">캐릭별 돌파/특성 재료</a></h1>
+    <h1><a href="index.py" title="캐릭별 돌파/특성 재료">캐릭별 돌파/특성 재료</a></h1>
   </div>
   <div id="grid">
 
     <ul class="table">
-      <a href="zhongli.html" title="종려">
-        <li id="zhongli">종려</li>
-      </a>
-      <a href="raiden-shogun.html" title="라이덴 쇼군">
-        <li id="raiden-shogun">라이덴 쇼군</li>
-      </a>
+      {CharList}
     </ul>
     <div id="article">
       <p>
-        <- 클릭하면 나옴
+      {desc}
       </p>
     </div>
   </div>
@@ -37,4 +50,4 @@ print('''<!DOCTYPE html>
 
 </body>
 </html>
-''')
+'''.format(char_name=page_id, CharList=CharList, desc=description))
